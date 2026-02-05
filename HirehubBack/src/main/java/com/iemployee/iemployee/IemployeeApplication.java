97569592d1987830a3fa218756cb25644058ee75
Yasin5949 <yasin2ashalo@gmail.com>
2026-02-05 10:59:18 -1200

    Initailized the BackEnd

---
+++ HirehubBack/src/main/java/com/iemployee/iemployee/IemployeeApplication.java
@@ -0,0 +1,13 @@
+package com.iemployee.iemployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IemployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IemployeeApplication.class, args);
+	}
+
+}
