0d2b9e937ba17f0ede29afd9f71394003557743a
Yasin5949 <yasin2ashalo@gmail.com>
2026-02-05 12:16:38 -1200

    updated te folders

---
+++ HirehubFront/src/Login.jsx
@@ -0,0 +1,11 @@
+import "./Login.module.css";
+function Login() {
+  return (
+    <div className="Container">
+      <form>
+        <h1>Hello!</h1>
+      </form>
+    </div>
+  );
+}
+export default Login;
