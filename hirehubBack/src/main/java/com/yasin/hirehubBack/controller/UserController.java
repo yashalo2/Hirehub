fbe5772d19345647acb9241010c1f2e2323c71da
Yasin5949 <yasin2ashalo@gmail.com>
2026-02-06 13:20:46 -1200

    configured  the backend

--- hirehubBack/src/main/java/com/yasin/hirehubBack/controller/UserController.java
+++ hirehubBack/src/main/java/com/yasin/hirehubBack/controller/UserController.java
@@ -1,4 +1,20 @@
 package com.yasin.hirehubBack.controller;
 
+import com.yasin.hirehubBack.model.User;
+import com.yasin.hirehubBack.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/Hirehub")
 public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/register")
+    public String register(@RequestBody User user) {
+        userRepository.save(user);
+        return "user registered";
+    }
 }
--- hirehubBack/src/main/java/com/yasin/hirehubBack/model/User.java
+++ hirehubBack/src/main/java/com/yasin/hirehubBack/model/User.java
@@ -1,4 +1,75 @@
 package com.yasin.hirehubBack.model;
-
+import jakarta.persistence.*;
+@Entity
+@Table(name="users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    @Column(unique=true,nullable=false)
+    private String email;
+    private String password;
+    private String role;
+
+    public User() {
+    }
+
+    public User(Long id, String firstName, String lastName, String email, String password, String role) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
