ba14a73b12c5362d318a379020cf75c5621b4584
Yasin5949 <yasin2ashalo@gmail.com>
2026-02-07 00:27:02 -1200

    made a login function

--- HirehubFront/src/Registration.jsx
+++ HirehubFront/src/Registration.jsx
@@ -1,8 +1,27 @@
+import { useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import style from "./Login.module.css";
 import Logo from "./assets/blackLogo.png";
+
 function Registration() {
+  const formRef = useRef(null);
   const navigate = useNavigate();
+
+  const Register = (e) => {
+    e.preventDefault();
+    const formData = new FormData(formRef.current);
+
+    fetch("http://localhost:8080/api/users/register", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.text())
+      .then((data) => {
+        console.log(data);
+        console.log(formData);
+      });
+  };
+
   return (
     <div className={style.Container}>
       <div className={style.header} onClick={() => navigate("/")}>
@@ -11,21 +30,21 @@ function Registration() {
       <div className={style.actionBtns}>
         <button onClick={() => navigate("/login")}>Login</button>
       </div>
-      <form>
+      <form ref={formRef} onSubmit={Register}>
         <h1>Sign-Up</h1>
-        <label htmlFor="email">First Name</label>
-        <input type="text" id="email" name="FirstName" />
-        <label htmlFor="email">Last Name</label>
-        <input type="text" id="email" name="ame" />
+        <label htmlFor="firstName">First Name</label>
+        <input type="text" id="firstName" name="firstName" />
+        <label htmlFor="lastName">Last Name</label>
+        <input type="text" id="lastName" name="lastName" />
         <label htmlFor="email">Email</label>
         <input type="text" id="email" name="email" />
         <label htmlFor="password">Password</label>
-        <input type="password" name="password" />
-        <label htmlFor="password"> Confirm Password</label>
-        <input type="password" name="password" />
-        <button>Sign-Up</button>
+        <input type="password" id="password" name="password" />
+        <label htmlFor="confirmPassword">Confirm Password</label>
+        <button type="submit">Sign-Up</button>
       </form>
     </div>
   );
 }
+
 export default Registration;
